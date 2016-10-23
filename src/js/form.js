4a94fbacbd6883e1b905ec6066aff84fd18e9f29
hlvs6 <hlvs6@yandex.ru>
2016-10-23 21:57:22 +0300

    Добавлена валидация для формы

--- src/js/form.js
+++ src/js/form.js
@@ -24,21 +24,38 @@ window.form = (function() {
     }
   };
 
-  var formReview = document.querySelector('.review-form');
-  var radiobuttons = formReview.elements('review-mark');
+  var inputName = document.querySelector('input[name="review-name"]');
+  var textAreaComment = document.querySelector('#review-text');
+  var radioButtons = document.querySelectorAll('input[name="review-mark"]');
+  var reviewFieldsName = document.querySelector('review-fields-name');
+  var reviewFieldsComment = document.querySelector('review-form-field-text');
+  var reviewFormControls = document.querySelector('review-form-controls');
+  var buttonSubmit = document.querySelector('review-submit');
 
-  //
-  // var radiobuttons = form.elements['review-mark'];
-  formReview.querySelector('.review-form-field-name').setAttribute('required', 'required');
-  //
-  for ( var i = 0; i < radiobuttons.length; i++) {
+  inputName.setAttribute('required', 'required');
+  reviewFieldsName.style.display = 'none';
+  reviewFieldsComment.style.display = 'none';
+  reviewFormControls.style.display = 'none';
 
-    if ( radiobuttons[i].value < 3) {
+  for ( var i = 0; i < radioButtons.length; i++) {
 
-      form.elements['review-text'].setAttribute('required');
+    if (radioButtons[i].hasAttribute('checked') && radioButtons[i].value < 3 ) {
+      textAreaComment.setAttribute('required', 'required');
     }
   }
 
+  if (inputName.value === '') {
+    reviewFieldsName.style.display = 'block';
+  }
+
+  if (textAreaComment.value === '' && textAreaComment.hasAttribute('required')) {
+    reviewFieldsComment.style.display = 'block';
+  }
+
+  if (inputName.value === '' || (textAreaComment.value === '' && textAreaComment.hasAttribute('required'))) {
+    reviewFormControls.style.display = 'block';
+    buttonSubmit.classList.add('disabled');
+  }
 
   formCloseButton.onclick = function(evt) {
     evt.preventDefault();
