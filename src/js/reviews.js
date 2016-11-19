a7823af9a1ee00b4545e41f813251c5fbb03016d
hlvs6 <hlvs6@yandex.ru>
2016-11-19 17:57:32 +0300

    Добавлен вывод по3 комментария при нажатии кнопки

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -1,10 +1,17 @@
 'use strict';
 define(['./loadmassive', './getComment'], function(loadReviews, Review) {
 
+  var selectorButtonMore = '.reviews-controls-more';
   var COMMENT_LOAD_URL = 'http://localhost:1507/api/reviews';
   var selectorListComment = '.reviews-list';
   var listComments = document.querySelector(selectorListComment);
-  loadReviews(COMMENT_LOAD_URL, { from: 1, to: 1, filter: 3}, function(loadedData) {
+  var buttonMore = document.querySelector(selectorButtonMore);
+  var currentPageNumber = 0;
+  var pageSize = 3;
+
+  buttonMore.classList.remove('invisible');
+
+  var renderComments = function(loadedData) {
 
     var selectorFilter = '.reviews-filter';
     var blockFilters = document.querySelector(selectorFilter);
@@ -19,5 +26,12 @@ define(['./loadmassive', './getComment'], function(loadReviews, Review) {
     });
 
     blockFilters.classList.remove(clsInvisible);
+  };
+
+  buttonMore.addEventListener('click', function(evt) {
+     currentPageNumber++;
+     loadReviews(COMMENT_LOAD_URL, { from: currentPageNumber * pageSize, to: currentPageNumber * pageSize + pageSize, filter: 3}, renderComments);
   });
+
+  loadReviews(COMMENT_LOAD_URL, { from: currentPageNumber * pageSize, to: currentPageNumber * pageSize + pageSize, filter: 3}, renderComments);
 });
