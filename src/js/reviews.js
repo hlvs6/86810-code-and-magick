23b52fc5da4b29f832fd5985f1c3a8da9f2a93ef
hlvs6 <hlvs6@yandex.ru>
2016-11-19 21:17:17 +0300

    Добавлен переключатель фильтров

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -1,20 +1,23 @@
 'use strict';
 define(['./loadmassive', './getComment'], function(loadReviews, Review) {
 
-  var selectorButtonMore = '.reviews-controls-more';
+
   var COMMENT_LOAD_URL = 'http://localhost:1507/api/reviews';
   var selectorListComment = '.reviews-list';
   var listComments = document.querySelector(selectorListComment);
+  var selectorButtonMore = '.reviews-controls-more';
   var buttonMore = document.querySelector(selectorButtonMore);
+  var selectorBlockFilter = '.reviews-filter';
+  var blockFilters = document.querySelector(selectorBlockFilter);
+  var selectorRadioFiltres = 'input[name="reviews"]';
+  var massiveFilters = blockFilters.querySelectorAll(selectorRadioFiltres);
+  massiveFilters = [].slice.call(massiveFilters);
   var currentPageNumber = 0;
   var pageSize = 3;
-
-  buttonMore.classList.remove('invisible');
+  var activeFilter = null;
 
   var renderComments = function(loadedData) {
 
-    var selectorFilter = '.reviews-filter';
-    var blockFilters = document.querySelector(selectorFilter);
     var clsInvisible = 'invisible';
     blockFilters.classList.add(clsInvisible);
 
@@ -28,10 +31,27 @@ define(['./loadmassive', './getComment'], function(loadReviews, Review) {
     blockFilters.classList.remove(clsInvisible);
   };
 
+  var changeFilter = function(filterID) {
+
+    listComments.innerHTML = '';
+    activeFilter = filterID;
+    currentPageNumber = 0;
+    loadReviews(COMMENT_LOAD_URL, { from: currentPageNumber * pageSize, to: currentPageNumber * pageSize + pageSize, filter: activeFilter}, renderComments);
+  };
+
+  blockFilters.addEventListener('click', function(evt){
+    if (evt.target.classList.contains('reviews-filter-item')) {
+      filterID = evt.target.getAttribute('for');
+      console.log(filterID);
+    }
+  }, true);
+
+  buttonMore.classList.remove('invisible');
+
   buttonMore.addEventListener('click', function(evt) {
      currentPageNumber++;
-     loadReviews(COMMENT_LOAD_URL, { from: currentPageNumber * pageSize, to: currentPageNumber * pageSize + pageSize, filter: 3}, renderComments);
+     loadReviews(COMMENT_LOAD_URL, { from: currentPageNumber * pageSize, to: currentPageNumber * pageSize + pageSize, filter: activeFilter}, renderComments);
   });
 
-  loadReviews(COMMENT_LOAD_URL, { from: currentPageNumber * pageSize, to: currentPageNumber * pageSize + pageSize, filter: 3}, renderComments);
+  loadReviews(COMMENT_LOAD_URL, { from: currentPageNumber * pageSize, to: currentPageNumber * pageSize + pageSize, filter: activeFilter}, renderComments);
 });
