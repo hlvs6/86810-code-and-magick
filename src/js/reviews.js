4a32b3fc251c5c423a076f949fb79cc8bbab94cf
hlvs6 <hlvs6@yandex.ru>
2016-11-19 15:45:13 +0300

    Добавлена отправка запросов на сервер с помощью XMLHttpRequest

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -4,14 +4,14 @@ define(['./loadmassive', './getComment'], function(loadReviews, Review) {
   var COMMENT_LOAD_URL = 'http://localhost:1507/api/reviews';
   var selectorListComment = '.reviews-list';
   var listComments = document.querySelector(selectorListComment);
-  loadReviews(COMMENT_LOAD_URL, function(data) {
+  loadReviews(COMMENT_LOAD_URL, { from: 1, to: 1, filter: 3}, function(loadedData) {
 
     var selectorFilter = '.reviews-filter';
     var blockFilters = document.querySelector(selectorFilter);
     var clsInvisible = 'invisible';
     blockFilters.classList.add(clsInvisible);
 
-    data.forEach( function(item) {
+    loadedData.forEach( function(item) {
       var comment = new Review(item);
       comment.addInfoOnTemplate();
       comment.addHandlerClick();
